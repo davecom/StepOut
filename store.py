2f1075c9eae1904de590c09ecd7592deb0aae887
Dave <snpukwana@gmail.com>
2018-03-18 04:29:14 -0400

    just testing

--- store.py
+++ store.py
@@ -3,6 +3,18 @@ import mailbox
 mailing_lists = ["kewgardens"]
 
 
+# Detect if a message is spam or not
+def is_spam(msg: mailbox.MaildirMessage) -> bool:
+    return False
+
+
+# Process an individual message
+# extract subject, author, just text (no mime attachments/html)
+def process(msg: mailbox.MaildirMessage):
+    for k, v in msg.items():
+        print(k, v)
+
+
 # Find all the text from each message and store it
 # along with the subject and author in Mongo
 # potentially check if went through spam filter first
@@ -11,7 +23,9 @@ mailing_lists = ["kewgardens"]
 def store_and_delete(mailing_list):
     md = mailbox.Maildir('/home/ubuntu/' + mailing_list + '/Maildir')
     for msg in md:
-        pass # extract subject, author, just text (no mime attachments/html)
+        # only look at new messages
+        if msg.get_subdir() == "new":
+            process(msg)
     return
 
 
